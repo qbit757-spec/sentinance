c89d5db34f108345f75524f8c28dd1a2d2480896
qbit <qbit757@gmail.com>
2026-06-27 09:02:55 -0500

    fix structlog TimeStamps to TimeStamper

--- app/core/logging.py
+++ app/core/logging.py
@@ -14,7 +14,7 @@ def setup_logging():
         processors=[
             structlog.stdlib.add_log_level,
             structlog.stdlib.add_logger_name,
-            structlog.processors.TimeStamps(fmt="iso"),
+            structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
             structlog.processors.JSONRenderer(),
